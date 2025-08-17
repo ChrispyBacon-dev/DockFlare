c39376301fc4ff9ad031a1bf65bf1b3ee46d5281
ChrispyBacon-dev <ori@hotmail.de>
2025-08-17 14:56:17 +0200

    deny=block decision

--- dockflare/app/web/routes.py
+++ dockflare/app/web/routes.py
@@ -1118,7 +1118,7 @@ def _parse_and_build_policy_from_form(email_str, ip_ranges_str=None, countries_l
         country_rules = [{"geo": {"country_code": country}} for country in countries_list]
         policies.insert(0, {
             "name": "Block selected countries",
-            "decision": "block",
+            "decision": "deny",
             "include": country_rules
         })
 
@@ -1130,7 +1130,7 @@ def _parse_and_build_policy_from_form(email_str, ip_ranges_str=None, countries_l
         })
         policies.append({
             "name": "Default Deny",
-            "decision": "block",
+            "decision": "deny",
             "include": [{"everyone": {}}]
         })
     elif countries_list:
